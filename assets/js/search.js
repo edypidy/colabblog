e93652735702c97a359749dfb67062cbe6f41871
edypidy <edypidy@users.noreply.github.com>
2021-09-19 06:23:18 +0000

    deploy: bd19c12a387c4a74d1ffe17a1447abc309f5791b

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/testcolabblog/assets/js/search-data.json', true);
+    request.open('GET', '/studyblog/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
